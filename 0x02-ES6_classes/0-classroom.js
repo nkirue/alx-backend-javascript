21aa436e15be53b4fa7f5002135d1893fe64ecc1
Nkiru Ezefosie <nkiru.happiness@gmail.com>
2024-04-11 18:35:53 +0000

    ES6 classes

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,7 @@
+// Define the ClassRoom class
+export default class ClassRoom {
+    constructor(maxStudentsSize) {
+        // Assign the maxStudentsSize to _maxStudentsSize
+        this._maxStudentsSize = maxStudentsSize;
+    }
+}
