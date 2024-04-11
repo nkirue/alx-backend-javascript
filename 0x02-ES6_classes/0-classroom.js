4af9f77dd57fa09dbbc1c65fe58398f202e810db
Nkiru Ezefosie <nkiru.happiness@gmail.com>
2024-04-11 21:12:00 +0000

    ES6 classes

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,7 +1,13 @@
-// Define the ClassRoom class
+/**
+ * Represents a class room.
+ */
 export default class ClassRoom {
-    constructor(maxStudentsSize) {
-        // Assign the maxStudentsSize to _maxStudentsSize
-        this._maxStudentsSize = maxStudentsSize;
-    }
+  /**
+   * Creates a new @see {@link ClassRoom}.
+   * @param {Number} maxStudentsSize - The maximum number of
+   * students in the class.
+   */
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
 }
