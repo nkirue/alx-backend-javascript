7269e055872a66426307778c3d0475d4afcf5bac
Nkiru Ezefosie <nkiru.happiness@gmail.com>
2024-04-11 21:30:37 +0000

    ES6 classes

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,9 +1,9 @@
 import ClassRoom from './0-classroom';
 
 /**
- * Creates an array of 3 ClassRoom with a specific size.
+ * Creates an array of 3 ClassRoom with a specific size
  * @returns an array of 3 ClassRoom objects.
  */
 export default function initializeRooms() {
   return [19, 20, 34].map((size) => new ClassRoom(size));
-} 
+}
