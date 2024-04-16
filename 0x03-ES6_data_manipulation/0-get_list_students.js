38ed11e6f96961d5a80b5c0e0de6b613a3b97e03
Nkiru Ezefosie <nkiru.happiness@gmail.com>
2024-04-16 12:32:23 +0000

    ES6 data manipulation

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,11 @@
+/**
+ * This retrieves a list of students.
+ * @returns an array of objects.
+ */
+export default function getListStudents() {
+  return [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
+}
