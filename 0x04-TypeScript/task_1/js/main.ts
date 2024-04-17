fabd41a2614a4472564282905ada2ddf080eead7
Nkiru Ezefosie <nkiru.happiness@gmail.com>
2024-04-17 13:57:57 +0000

    Typescript

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,10 @@
+// task_1
+
+export interface Teacher {
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  location: string;
+  [key: string]: any; // Allow any additional attribute
+}
